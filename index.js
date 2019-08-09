335a48ae7aba6b3cbde983677273fc382c4db84c
AlexVorn-ПК\AlexVorn <voroncov.sasha93@gmail.com>
2019-08-09 16:23:20 +0300

    2-3 parth

---
+++ index.js
@@ -0,0 +1,25 @@
+//parth 2
+var calculator = {
+    read : function ( x, y ) {
+        x = prompt(),
+        y = prompt();
+        return (x, y)
+    },
+    sum : function ( x, y ) {
+          this.x + this.y 
+    }
+};
+
+calculator.read();
+console.log(calculator.sum());
+
+//parth 3
+// var user = {
+//     name : "Tom"
+// };
+
+// function format(beginMsg, endMsg) {
+//     console.log(beginMsg + this.name + endMsg);
+// }
+
+// var tomFormat = 
